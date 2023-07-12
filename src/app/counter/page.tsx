c28990a2ae9fac3dbab7be66972c6b3c5a57eef7
manuelWeb <devries.webdesigner@gmail.com>
2023-07-12 11:59:26 +0200

    **feat** add counter page

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,12 @@
+import Counter from '@/components/Counter'
+
+const page = () => {
+  return (
+    <div>
+      Hello counter page <br />
+      <Counter />
+    </div>
+  );
+};
+
+export default page;
